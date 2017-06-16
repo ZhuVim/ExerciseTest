f77a15040ff3d1bfb7994be33096e021a8817bf6
Wuming <Wuming@Wuming-PC>
2017-06-16 22:20:52 +0800

    远程提交

--- src/E03_OutBoundsException.java
+++ src/E03_OutBoundsException.java
@@ -1,16 +1,15 @@
-import java.lang.reflect.AnnotatedArrayType;
 
 public class E03_OutBoundsException {
-	public static void main(String[] args) {
-		int i;
-		int[] array={1,2,3,4};
-		try {
-			for(i=0;i<5;i++)
-			array[i]=i;
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e);
-		}
-	}
+    public static void main(String[] args) {
+        int i;
+        int[] array = { 1, 2, 3, 4 };
+        try {
+            for (i = 0; i < 5; i++)
+                array[i] = i;
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+    }
 
 }
