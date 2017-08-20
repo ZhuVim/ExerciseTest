d91297b0daeea4de4df273d2d828c202156cc3ed
Wuming <zhuwumingemail@gmail.com>
2017-08-20 23:41:12 +0800

    新增slist

---
+++ src/com/containers/SListIterator.java
@@ -0,0 +1,11 @@
+package com.containers;
+
+public interface SListIterator<T> {
+    boolean hasNext();
+
+    T next();
+
+    void remove();
+
+    void add(T element);
+}
--- src/test1/Test.java
+++ src/test1/Test.java
@@ -1,17 +1,13 @@
 package test1;
-import java.awt.SystemTray;
-import java.util.Date;
+
 public class Test {
-	public static void main(String[] args) {
-		byte[] a = new byte[255];
-		for(int i=0;i<255;i++)
-			a[i]=(byte) i;
-		short v=(short)a[37+0x80];
-		System.out.println((short)(37+0x80)&0x00FF);
-		System.out.println(v);
-		short c =200;
-		float f=1.6f;
-		System.out.println("float= "+(int)f);
-	}
+    public static void main(String[] args) {
+        Object c = null;
+        String ccString = "null";
+        System.out.println(ccString);
+        //        System.out.println(String.valueOf(c));
+        //        System.out.println(null == String.valueOf(c));
+
+    }
 
 }
